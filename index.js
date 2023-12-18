5f5fbf0f33591585162db0b3164a5546a5cf5054
Jana May <jana.ricarda.may@gmail.com>
2023-12-18 14:46:10 +0100

    challenge 1 and 2

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD === receivedPassword) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
 // Part 2: Even / Odd
 const number = 6;
+
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
